e8a29169d26ad1019bda25adb63bdaedbc742f10
publicdbhost <jgt@cock.li>
2017-06-23 22:29:36 +0200

    Create lang_no.php

---
+++ lang_no.php
@@ -0,0 +1,60 @@
+<?php
+// language: Norsk
+// made by: Snusbase Translate Team
+// updated on: 22.June.2017
+
+// MENU
+define('_SEARCH', 'søk');
+define('_PURCHASE', 'kjøp');
+define('_SUPPORT', 'kundestøtte');
+define('_ACCOUNT', 'bruker');
+define('_ACCOUNT_SETTINGS', 'brukerinstillinger');
+define('_ACCOUNT_CHANGE', 'Sideinstillinger'); //client: website, eg font type, theme
+define('_AFFILIATE', 'affiliate');
+define('_AFFILIATE_NEW', 'se fortjeneste');
+define('_AFFILIATE_KEYS', 'endre nøkler');
+
+// ACCOUNT
+define('_SIGN_OUT', 'logg ut');
+define('_WELCOME', 'velkommen,'); //eg Welcome, Username.
+
+// SUPPORT
+define('_SUPPORT', 'alternative måter å ta kontakt er twitteren- og emailen våres; publicdbhost@gmail.com');
+define('_MOST_ACTIVE', 'våre mest aktive kundestøtte tider er');
+define('_MONDAY_FRIDAY', 'Mandag til Fredag.');
+
+// MEMBERS PAGE
+define('_P_OVERVIEW', 'ytelsesoversikt');
+define('_AVG_QUERY', 'gjennomsnitts query');
+define('_AVG_RESULTS', 'gjennomsnittlige resultater');
+define('_ACC_NUMBER', 'bruker nommer');
+define('_API_LOAD', 'api last');
+define('_SHORT_TERM', 'kort-tids mål');
+define('_LONG_TERM', 'langtids mål');
+define('_MOST_ACTIVE', 'Vi prøver hele tiden å forbedre ytelse, på grunn av dette gjør vi stattestikkene deres offentlige for hvem som helst. Disse tilbakestilles på starten av hver måned og hver gang vi oppdaterer APIen våres');
+
+// ACCOUNT PAGE
+define('_CHANGE_USERNAME', 'endre språk');
+define('_CHANGE_USERNAME_DISPLAY', 'endre visningsnavn');
+define('_CHANGE_EMAIL', 'endre email');
+define('_CHANGE_PASSWORD', 'endre passord');
+
+// PURCHASE PAGE
+define('_PURCHASE', 'kjøp');
+define('_X_DAY_ACCESS', 'dager tilgang'); //EG "7 DAY ACCESS"
+define('_SEARCH_FEATURES', 'uendelig tilgang til søketjenester');
+define('_BASIC_WILDCARD', 'basisk wildcard');
+define('_CUSTOM_WILDCARD', 'egendefinert wildcard');
+define('_PRIORITIZED_SUPPORT', 'prioritert støtte');
+define('_PRIVATE_ENDPOINT', 'privat API endepunkt');
+define('_ALL_FEATURES', 'all egenskapene av Demo og Premium');
+define('_DAY_EMAIL_SUPPORT', '24/7 email støtte');
+
+// AFFILIATE PAGE
+define('_AFFILIATE_WELCOME', 'affiliering system');
+define('_AFFILIATE_MADE', 'penger tjent');
+define('_AFFILIATE_KEY', 'affilierings nøkkel');
+define('_AFFILIATE_CREATE', 'generer ny nøkker');
+define('_AFFILIATE_WITHDRAW', 'ta ut');
+
+?>
