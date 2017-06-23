42e173d595c69886e2a05271853e890a79c91e2c
cheater <cheater00@gmail.com>
2017-06-23 23:53:01 +0200

    German translation

---
+++ lang_de.php
@@ -0,0 +1,60 @@
+<?php
+// language: English
+// made by: Snusbase Translate Team
+// updated on: 22.June.2017
+
+// MENU
+define('_SEARCH', 'Suche');
+define('_PURCHASE', 'kaufen');
+define('_SUPPORT', 'Support');
+define('_ACCOUNT', 'Konto');
+define('_ACCOUNT_SETTINGS', 'Kontoeinstellungen');
+define('_ACCOUNT_CHANGE', 'Benützereinstellungen'); //client: website, eg font type, theme
+define('_AFFILIATE', 'Partnerprogramm');
+define('_AFFILIATE_NEW', 'Einkünfte');
+define('_AFFILIATE_KEYS', 'Schlüsseländerung');
+
+// ACCOUNT
+define('_SIGN_OUT', 'abmelden');
+define('_WELCOME', 'hallo,'); //eg Welcome, Username.
+
+// SUPPORT
+define('_SUPPORT', 'zusätzlich können Sie mit uns auf Twitter und unter publicdbhost@gmail.com Kontakt aufnehmen');
+define('_MOST_ACTIVE', 'unser Support ist verfügbar während dieser Zeiten');
+define('_MONDAY_FRIDAY', 'Montag bist Freitag.');
+
+// MEMBERS PAGE
+define('_P_OVERVIEW', 'Leistung im überblick');
+define('_AVG_QUERY', 'durchschnittliche Abfrage');
+define('_AVG_RESULTS', 'durchschnittliches Suchergebnis');
+define('_ACC_NUMBER', 'kontonummer');
+define('_API_LOAD', 'last auf das API');
+define('_SHORT_TERM', 'kurzfristige Ziele');
+define('_LONG_TERM', 'langfristige Ziele');
+define('_MOST_ACTIVE', 'Wir versuchen es ständig, unsere Leistung zu verbessern. Deswegen machen wir unsere Statistiken frei verfügbar. Diese werden am anfang jedes Monats zurückgesetzt, wenn wir größere Änderungen in unserem API vornehmen.');
+
+// ACCOUNT PAGE
+define('_CHANGE_USERNAME', 'Benutzername ändern');
+define('_CHANGE_USERNAME_DISPLAY', 'Angezeigte Namen ändern');
+define('_CHANGE_EMAIL', 'Emailaddresse ändern');
+define('_CHANGE_PASSWORD', 'Passwort ändern');
+
+// PURCHASE PAGE
+define('_PURCHASE', 'kaufen');
+define('_X_DAY_ACCESS', 'tag(e) Zugang'); //EG "7 DAY ACCESS"
+define('_SEARCH_FEATURES', 'unbegrenzter Zugriff zu Suchfunktionen');
+define('_BASIC_WILDCARD', 'einfaches Wildcard');
+define('_CUSTOM_WILDCARD', 'spezielles Wildcard');
+define('_PRIORITIZED_SUPPORT', 'Vorzugs-support');
+define('_PRIVATE_ENDPOINT', 'private API-Anbindung');
+define('_ALL_FEATURES', 'alle Funktionen von Demo und Premium');
+define('_DAY_EMAIL_SUPPORT', 'Email-support rund um die Uhr');
+
+// AFFILIATE PAGE
+define('_AFFILIATE_WELCOME', 'Partnerprogramm');
+define('_AFFILIATE_MADE', 'Einnahmen');
+define('_AFFILIATE_KEY', 'Partnerschlüssel');
+define('_AFFILIATE_CREATE', 'neuen Schlüssel generieren');
+define('_AFFILIATE_WITHDRAW', 'Fonds abheben');
+
+?>
