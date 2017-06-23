cbd7840cc36386ebae334f8a2103569c7fbee6d0
publicdbhost <jgt@cock.li>
2017-06-23 22:10:07 +0200

    Create lang_en.php
    The English translation for snusbase.com

---
+++ lang_en.php
@@ -0,0 +1,60 @@
+<?php
+// language: English
+// made by: Snusbase Translate Team
+// updated on: 22.June.2017
+
+// MENU
+define('_SEARCH', 'search');
+define('_PURCHASE', 'purchase');
+define('_SUPPORT', 'support');
+define('_ACCOUNT', 'account');
+define('_ACCOUNT_SETTINGS', 'account settings');
+define('_ACCOUNT_CHANGE', 'client settings'); //client: website, eg font type, theme
+define('_AFFILIATE', 'affiliate');
+define('_AFFILIATE_NEW', 'view earnings');
+define('_AFFILIATE_KEYS', 'change keys');
+
+// ACCOUNT
+define('_SIGN_OUT', 'sign out');
+define('_WELCOME', 'welcome,'); //eg Welcome, Username.
+
+// SUPPORT
+define('_SUPPORT', 'alternative methods of contact are our twitter and our email address publicdbhost@gmail.com');
+define('_MOST_ACTIVE', 'our most active support hours are');
+define('_MONDAY_FRIDAY', 'Monday thru Friday.');
+
+// MEMBERS PAGE
+define('_P_OVERVIEW', 'performance overview');
+define('_AVG_QUERY', 'average query');
+define('_AVG_RESULTS', 'average results');
+define('_ACC_NUMBER', 'account number');
+define('_API_LOAD', 'api load');
+define('_SHORT_TERM', 'short-term goals');
+define('_LONG_TERM', 'long-term goals');
+define('_MOST_ACTIVE', 'We constantly strive to improve performance, because of this we are making our statistics public for anyone to see. These reset at the start of every month and when we make major API updates.');
+
+// ACCOUNT PAGE
+define('_CHANGE_USERNAME', 'change username');
+define('_CHANGE_USERNAME_DISPLAY', 'change display names');
+define('_CHANGE_EMAIL', 'change email');
+define('_CHANGE_PASSWORD', 'change password');
+
+// PURCHASE PAGE
+define('_PURCHASE', 'purchase');
+define('_X_DAY_ACCESS', 'day access'); //EG "7 DAY ACCESS"
+define('_SEARCH_FEATURES', 'unlimited access to search features');
+define('_BASIC_WILDCARD', 'basic wildcard');
+define('_CUSTOM_WILDCARD', 'custom wildcard');
+define('_PRIORITIZED_SUPPORT', 'prioritized support');
+define('_PRIVATE_ENDPOINT', 'private API endpoint');
+define('_ALL_FEATURES', 'all features of Demo and Premium');
+define('_DAY_EMAIL_SUPPORT', '24/7 email support');
+
+// AFFILIATE PAGE
+define('_AFFILIATE_WELCOME', 'affiliate system');
+define('_AFFILIATE_MADE', 'money made');
+define('_AFFILIATE_KEY', 'affiliate key');
+define('_AFFILIATE_CREATE', 'create new key');
+define('_AFFILIATE_WITHDRAW', 'withdraw');
+
+?>
