0e1d32247d5dee75cfba65a2ce4ec53cba81c488
DeshawnTyroneWilliams <deshawnwilliams@cock.li>
2017-06-25 18:27:16 +0930

    Create lang_african_american.php

---
+++ lang_african_american.php
@@ -0,0 +1,60 @@
+<?php
+// language: African American
+// made by: Deshawn Tyrone Williams
+// updated on: 25.June.2017
+
+// MENU
+define('_SEARCH', 'look fo');
+define('_PURCHASE', 'gibs me dat');
+define('_SUPPORT', 'help a nigga out');
+define('_ACCOUNT', 'account');
+define('_ACCOUNT_SETTINGS', 'how yo account be');
+define('_ACCOUNT_CHANGE', 'how yo website be'); //client: website, eg font type, theme
+define('_AFFILIATE', 'trap nigga');
+define('_AFFILIATE_NEW', 'get dat money');
+define('_AFFILIATE_KEYS', 'gibs me dat keys');
+
+// ACCOUNT
+define('_SIGN_OUT', 'da police is here');
+define('_WELCOME', 'wassup,'); //eg Welcome, Username.
+
+// SUPPORT
+define('_SUPPORT', 'ay nigga hit me up on the twitter or at my email publicdbhost@gmail.com');
+define('_MOST_ACTIVE', 'the best time to reach our team of real niggas iz');
+define('_MONDAY_FRIDAY', 'Monday thru Friday.');
+
+// MEMBERS PAGE
+define('_P_OVERVIEW', 'how good da performance iz and shit');
+define('_AVG_QUERY', 'how long da queryz take');
+define('_AVG_RESULTS', 'how many results dey iz');
+define('_ACC_NUMBER', 'account number');
+define('_API_LOAD', 'api load');
+define('_SHORT_TERM', 'what we be tryna do and shit');
+define('_LONG_TERM', 'what we be tryna do later n shit');
+define('_MOST_ACTIVE', 'shit nigga, we all about transparency n shit, you feel me? we put deez statistics n shit up here cuhz, dey reset on da first of da month');
+
+// ACCOUNT PAGE
+define('_CHANGE_USERNAME', 'buy fake id');
+define('_CHANGE_USERNAME_DISPLAY', 'paint over existing fake id');
+define('_CHANGE_EMAIL', 'change yo contact n shit');
+define('_CHANGE_PASSWORD', 'change yo password');
+
+// PURCHASE PAGE
+define('_PURCHASE', 'shit, gibs me dat');
+define('_X_DAY_ACCESS', 'you can use dis shit fo dat many days'); //EG "7 DAY ACCESS"
+define('_SEARCH_FEATURES', 'you be out here searchin all you want nigga');
+define('_BASIC_WILDCARD', 'basic wildcard');
+define('_CUSTOM_WILDCARD', 'custom wildcard');
+define('_PRIORITIZED_SUPPORT', 'shit, you be the vip nigga');
+define('_PRIVATE_ENDPOINT', 'get yo own hosting n shit');
+define('_ALL_FEATURES', 'you be tha big baller nigga with all the features');
+define('_DAY_EMAIL_SUPPORT', 'you can hit our team our highly trained hood niggaz up any time you like');
+
+// AFFILIATE PAGE
+define('_AFFILIATE_WELCOME', 'trap nigga system');
+define('_AFFILIATE_MADE', 'how much money u got');
+define('_AFFILIATE_KEY', 'trap nigga key');
+define('_AFFILIATE_CREATE', 'gibs me dat new key');
+define('_AFFILIATE_WITHDRAW', 'gibs me dat money nigga');
+
+?>
