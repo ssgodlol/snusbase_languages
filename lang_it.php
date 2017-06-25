422d1772c9971c32dbf216db7a7235a53e97daa4
Abdel Adim `smaury` Oisfi <maury9219@gmail.com>
2017-06-25 20:13:34 +0200

    Italian Translation

---
+++ lang_it.php
@@ -0,0 +1,59 @@
+<?php
+// language: Italian
+// made by: Snusbase Translate Team
+// updated on: 22.June.2017
+
+// MENU
+define('_SEARCH', 'cerca');
+define('_PURCHASE', 'acquista');
+define('_SUPPORT', 'supporto');
+define('_ACCOUNT', 'profilo');
+define('_ACCOUNT_SETTINGS', 'impostazioni profilo');
+define('_ACCOUNT_CHANGE', 'impostazioni sito'); //client: website, eg font type, theme
+define('_AFFILIATE', 'affiliazione');
+define('_AFFILIATE_NEW', 'mostra guadagni');
+define('_AFFILIATE_KEYS', 'modifica chiavi');
+
+// ACCOUNT
+define('_SIGN_OUT', 'esci');
+define('_WELCOME', 'benvenuto,'); //eg Welcome, Username.
+
+// SUPPORT
+define('_SUPPORT', 'i metodi alternativi di comunicazione sono il nostro account twitter e la nostra e-mail publicdbhost@gmail.com');
+define('_MOST_ACTIVE', 'le nostre ore di supporto più attive sono');
+define('_MONDAY_FRIDAY', 'Da lunedì a venerdì.');
+
+// MEMBERS PAGE
+define('_P_OVERVIEW', 'panoramica delle prestazioni');
+define('_AVG_QUERY', 'media query');
+define('_AVG_RESULTS', 'media risultati');
+define('_ACC_NUMBER', 'numero account');
+define('_API_LOAD', 'carico api');
+define('_SHORT_TERM', 'obiettivi nel breve termine');
+define('_LONG_TERM', 'obiettivi nel lungo termine');
+define('_MOST_ACTIVE', 'Noi cerchiamo costantemente di migliorare il nostro servizio, perciò le nostre statistiche sono pubbliche. Queste vengono resettate ogni mese e ad ogni update maggiore delle API.');
+
+// ACCOUNT PAGE
+define('_CHANGE_USERNAME', 'cambia username');
+define('_CHANGE_USERNAME_DISPLAY', 'cambia nomi visualizzati');
+define('_CHANGE_EMAIL', 'cambia email');
+define('_CHANGE_PASSWORD', 'cambia password');
+
+// PURCHASE PAGE
+define('_PURCHASE', 'acquista');
+define('_X_DAY_ACCESS', 'giorni d\'accesso'); //EG "7 DAY ACCESS"
+define('_SEARCH_FEATURES', 'accesso illimitato alle funzionalità di ricerca');
+define('_BASIC_WILDCARD', 'jolly basico');
+define('_CUSTOM_WILDCARD', 'jolly personalizzato');
+define('_PRIORITIZED_SUPPORT', 'supporto prioritario');
+define('_PRIVATE_ENDPOINT', 'endpoint API privati');
+define('_ALL_FEATURES', 'tutte le funzionalità Demo e Premium');
+define('_DAY_EMAIL_SUPPORT', 'support email 24/7');
+// AFFILIATE PAGE
+define('_AFFILIATE_WELCOME', 'sistema di affiliazione');
+define('_AFFILIATE_MADE', 'soldi guadagnati');
+define('_AFFILIATE_KEY', 'chiave affiliazione');
+define('_AFFILIATE_CREATE', 'crea una nuova chiave');
+define('_AFFILIATE_WITHDRAW', 'ritirare');
+
+?>
